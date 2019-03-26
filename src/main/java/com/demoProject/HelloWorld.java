ee54f9f3e9c80622961ee009d4f18ad53bb008c3
ankita gupta <agcutiepie24@gmail.com>
2019-03-27 01:09:25 +0530

    Add files

---
+++ src/main/java/com/demoProject/HelloWorld.java
@@ -0,0 +1,10 @@
+import org.joda.time.LocalTime;
+public class HelloWorld
+{
+	public static void main(String args[])
+	{
+		LocalTime time=new LocalTime();
+		System.out.println("Local Time is: "+ time);
+		System.out.println("This is a Hello World project");
+	}
+}
