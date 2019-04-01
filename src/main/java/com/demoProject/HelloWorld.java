a33e4c69f773c67549cb40d9bc2bedec5c29e790
ankita gupta <agcutiepie24@gmail.com>
2019-04-01 18:50:38 +0530

    Adding files for Java project

---
+++ src/main/java/com/demoProject/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.demoProject
+
+public class HelloWorld
+{
+	public void printMsg()
+	{
+		System.out.println("This is a Hello World project");
+	}
+
+}
---
+++ src/test/java/com/demoProject/TestHelloWorld.java
@@ -0,0 +1,26 @@
+package com.demoProject
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.After;
+
+    
+public class TestHelloWorld
+{
+	
+	@ActualTest
+	public void printTestMsg()
+	{
+		System.out.println("This is a Test Msg");
+		HelloWorld obj=new HelloWorld()
+		
+		assertEquals(obj.printMsg(),"This is a Hello World project");
+	}
+
+	@After
+         public void after() 
+	{
+        	System.out.println("After Test Case");
+    	}
+}
